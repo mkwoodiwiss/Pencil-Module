94d6c7e519c5c7695030919d9f68075470f30115
mkwoodiwiss <mwoodiwiss@carollo.com>
2025-06-26 15:29:29 -0600

    Add automated filtration features and tests

--- tests/simulated_hardware.py
+++ tests/simulated_hardware.py
@@ -5,11 +5,17 @@ class FakeSerial:
         self.baud = baud
         self.timeout = timeout
         self._buffer = b""
+        self.commands = []
 
     def write(self, data: bytes):
-        # The real system expects a 'P' command to read weight
+        self.commands.append(data)
+        # The real system expects commands for each scale and zeroing
         if data == b"P\r\n":
             self._buffer = b"+123.45 g\r\n"
+        elif data == b"S\r\n":
+            self._buffer = b"+54.32 g\r\n"
+        elif data in (b"Z\r\n", b"Q\r\n"):
+            self._buffer = b"OK\r\n"
 
     def read_until(self, sep: bytes = b"\r\n") -> bytes:
         response = self._buffer or b""
