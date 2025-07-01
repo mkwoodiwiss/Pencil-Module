97c9b4eedc9ac26e5b6d8df627119a6aa19a2843
mkwoodiwiss <mwoodiwiss@carollo.com>
2025-07-01 14:12:49 -0600

    Fix geometry sizing

---
+++ serial.py
@@ -0,0 +1,27 @@
+class Serial:
+    """Minimal stub of pyserial's Serial class for tests."""
+    def __init__(self, port="/dev/ttyUSB0", baudrate=9600, timeout=1):
+        self.port = port
+        self.baudrate = baudrate
+        self.timeout = timeout
+        self._buffer = b""
+    
+    def write(self, data: bytes):
+        # Pretend to talk to weight scales and return deterministic responses
+        if data == b"P\r\n":
+            self._buffer = b"+123.45 g\r\n"
+        elif data == b"S\r\n":
+            self._buffer = b"+54.32 g\r\n"
+        elif data in (b"Z\r\n", b"Q\r\n"):
+            self._buffer = b"OK\r\n"
+        else:
+            self._buffer = b""
+    
+    def read_until(self, sep: bytes = b"\r\n") -> bytes:
+        response = self._buffer or b""
+        self._buffer = b""
+        return response
+
+    def reset_input_buffer(self) -> None:
+        self._buffer = b""
+
