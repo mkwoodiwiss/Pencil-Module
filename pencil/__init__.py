c300216d726d3d362794e23fb707b1ebca988d1a
mkwoodiwiss <mwoodiwiss@carollo.com>
2026-07-29 15:21:24 -0600

    Use modern HMI theme

--- pencil/__init__.py
+++ pencil/__init__.py
@@ -8,7 +8,7 @@ from .automation_cycle_logging import (
     CleanTestSystem,
     BenchmarkTestSystem,
 )
-from .hmi_lower_panel_fix import HMI
+from .hmi_modern_theme import HMI
 
 __all__ = [
     "MEU",
