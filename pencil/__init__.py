f0fada08432bef5dfedc23e8258b1d1ca5b9d03a
mkwoodiwiss <mwoodiwiss@carollo.com>
2026-07-30 12:35:59 -0600

    Use final Clean settings geometry correction

--- pencil/__init__.py
+++ pencil/__init__.py
@@ -8,7 +8,7 @@ from .automation_cycle_logging import (
     CleanTestSystem,
     BenchmarkTestSystem,
 )
-from .hmi_touchscreen import HMI
+from .hmi_clean_match import HMI
 
 __all__ = [
     "MEU",
