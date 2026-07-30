ecc3fb786bf05b02854ed1c42483d0470d2c5176
mkwoodiwiss <mwoodiwiss@carollo.com>
2026-07-30 13:19:23 -0600

    Keep final HMI export stable

--- pencil/__init__.py
+++ pencil/__init__.py
@@ -8,7 +8,7 @@ from .automation_cycle_logging import (
     CleanTestSystem,
     BenchmarkTestSystem,
 )
-from .hmi_settings_dialog_fit import HMI
+from .hmi_clean_match import HMI
 
 __all__ = [
     "MEU",
