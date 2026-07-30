87565013ac73c247b31f46dfe656c150d9c14746
mkwoodiwiss <mwoodiwiss@carollo.com>
2026-07-30 13:16:55 -0600

    Use fitted settings dialog HMI

--- pencil/__init__.py
+++ pencil/__init__.py
@@ -8,7 +8,7 @@ from .automation_cycle_logging import (
     CleanTestSystem,
     BenchmarkTestSystem,
 )
-from .hmi_clean_match import HMI
+from .hmi_settings_dialog_fit import HMI
 
 __all__ = [
     "MEU",
