f66a30d9d563b6468ee05b4e7b3ccdbb197d5553
mkwoodiwiss <mwoodiwiss@carollo.com>
2026-07-31 09:16:06 -0600

    Use Test-identical Post-Scrub settings dialog

--- pencil/__init__.py
+++ pencil/__init__.py
@@ -8,7 +8,7 @@ from .automation_cycle_logging import (
     CleanTestSystem,
     BenchmarkTestSystem,
 )
-from .hmi_v2_tk_compat import HMI
+from .hmi_v2_post_scrub_dialog import HMI
 
 __all__ = [
     "MEU",
