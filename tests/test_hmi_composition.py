9a610a57396841a1290a6b1bfd9a5ce903ca08c4
mkwoodiwiss <mwoodiwiss@carollo.com>
2026-07-31 10:50:21 -0600

    Test flattened Tk clone composition

--- tests/test_hmi_composition.py
+++ tests/test_hmi_composition.py
@@ -6,8 +6,9 @@ from pencil.hmi_filtration_dialogs import FiltrationSettingsDialogMixin
 from pencil.hmi_identifier_state import IdentifierStateMixin
 from pencil.hmi_post_scrub_state import PostScrubStateMixin
 from pencil.hmi_summary_formatting import SummaryFormattingMixin
+from pencil.hmi_tk_clone_compat import TkCloneCompatibilityMixin
+from pencil.hmi_v2_clone_test_layout import HMI as CloneLayoutHMI
 from pencil.hmi_v2_integrated import HMI
-from pencil.hmi_v2_tk_compat import HMI as V2TkCompatHMI
 
 
 class TestHMIComposition(unittest.TestCase):
@@ -17,12 +18,14 @@ class TestHMIComposition(unittest.TestCase):
         self.assertIn(IdentifierStateMixin, mro)
         self.assertIn(PostScrubStateMixin, mro)
         self.assertIn(SummaryFormattingMixin, mro)
-        self.assertIn(V2TkCompatHMI, mro)
+        self.assertIn(TkCloneCompatibilityMixin, mro)
+        self.assertIn(CloneLayoutHMI, mro)
 
     def test_obsolete_patch_classes_are_not_in_production_mro(self):
         module_names = {base.__module__ for base in HMI.__mro__}
         self.assertNotIn("pencil.hmi_v2_post_scrub_dialog", module_names)
         self.assertNotIn("pencil.hmi_v2_summary_text", module_names)
+        self.assertNotIn("pencil.hmi_v2_tk_compat", module_names)
 
     def test_sample_identifier_rename_leaves_sample_time_unchanged(self):
         text = "Sample Time: 1 s\nSample: ABC"
